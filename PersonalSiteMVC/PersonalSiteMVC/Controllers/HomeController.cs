38f21f1324348014c86988782f97228520d5f33c
BlaineG106 <blaineg3291@gmail.com>
2020-12-14 10:01:20 -0600

    Finished contact form

--- PersonalSiteMVC/PersonalSiteMVC/Controllers/HomeController.cs
+++ PersonalSiteMVC/PersonalSiteMVC/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using PersonalSiteMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,13 +16,63 @@ namespace PersonalSiteMVC.Controllers
             return View();
         }
 
-       
 
+        [HttpGet]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            string body = $"Message from {cvm.Name} about {cvm.Subject}. There Email: {cvm.Email} <br> Message: <br> {cvm.Message}";
+
+            MailMessage msg = new MailMessage("admin@blainegomez.com", "blaineg3291@gmail.com", "Email from blainegomez.com", body);
+            msg.IsBodyHtml = true;
+
+            SmtpClient client = new SmtpClient("mail.blainegomez.com");
+            client.Credentials = new NetworkCredential("admin@blainegomez.com", "Gomez644!");
+            client.Port = 8889;
+
+            try
+            {
+                client.Send(msg);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.SendEmailError = $"Uh oh, something went wrong. Details: {ex.Message}";
+                return View(cvm);
+            }
+
+
+            return View("EmailConfirmation", cvm);
+        }
+        //Email Confirmation Page Test
+        
+        //public ActionResult Tester()
+        //{
+
+        //    ContactViewModel cvm = new ContactViewModel();
+
+        //    cvm.Name = "Fake Name";
+
+        //    cvm.Email = "Fake Email";
+
+        //    return View("EmailConfirmation", cvm);
+
+        //}
     }
+
+    
+
+
 }
---
+++ PersonalSiteMVC/PersonalSiteMVC/Models/ContactViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PersonalSiteMVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "Name is Required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Email is Required")]
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "Message is Required")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+    }
+}
